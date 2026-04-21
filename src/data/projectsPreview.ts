640f32b44f4062a143510dd20c0d7bbddfa266df
Joaquin-Elia <joaquin.elia@hotmail.com>
2026-04-21 19:04:37 -0300

    feat: add videos to projects previews

---
+++ src/data/projectsPreview.ts
@@ -0,0 +1,32 @@
+const projectsPreview = [
+  {
+    name: "VIRUS 2000",
+    imageSrc: "/images/1.jpeg",
+    videoSrc: "/videos/Virus-Previsualizacion.mp4",
+    videoStyle: "max-w-96",
+    wrapperClass: "lg:relative",
+  },
+  {
+    name: "FLYEMIGRATES",
+    imageSrc: "/images/2.jpeg",
+    videoSrc: "/videos/Pre-Fly.mp4",
+    videoStyle: "max-w-96",
+    wrapperClass: "lg:absolute lg:ml-20 lg:left-[14rem] lg:top-0",
+  },
+  {
+    name: "TUMBANDANDO EL CLUB",
+    imageSrc: "/images/3.jpeg",
+    videoSrc: "/videos/Previsualizacion.mp4",
+    videoStyle: "max-w-120",
+    wrapperClass: "lg:-ml-1 lg:-mt-0.5",
+  },
+  {
+    name: "CERRO CATEDRAL",
+    imageSrc: "/images/4.jpeg",
+    videoSrc: "/videos/pre-catedral.mp4",
+    videoStyle: "max-w-130",
+    wrapperClass: "lg:-mt-0.5 lg:ml-64",
+  },
+];
+
+export default projectsPreview;
