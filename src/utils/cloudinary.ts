ab0fad081da555553c0e6682d118ff50f15f3ecd
Joaquin-Elia <joaquin.elia@hotmail.com>
2026-04-26 19:36:39 -0300

    feat: add ResponsiveVideo component

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -4,3 +4,11 @@ export const cld = (src: string, t: string) => {
   return `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/${t}/${src}`;
 };
 
+export const cldVideo = (src: string, t: string) => {
+  return `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/${t}/${src}`;
+};
+
+export const cldPoster = (src: string, t: string) => {
+  const publicId = src.split(".")[0];
+  return `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/${t}/${publicId}.jpg`;
+};
