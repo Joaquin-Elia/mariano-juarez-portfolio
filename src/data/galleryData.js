14097b2daeefbade09146642c5ca9c7cb3dd12c0
Joaquin-Elia <joaquin.elia@hotmail.com>
2026-05-04 18:14:12 -0300

    feat: add full assets to gallery

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -3,7 +3,21 @@ const galleryImgs = [
     {img: "https://res.cloudinary.com/dpm7cqzhq/image/upload/q_auto/f_auto/v1777587235/Stills13_fkcea9.webp"},
     {img: "https://res.cloudinary.com/dpm7cqzhq/image/upload/q_auto/f_auto/v1777587231/stills12_iwke2o.webp"},
     {img: "https://res.cloudinary.com/dpm7cqzhq/image/upload/q_auto/f_auto/v1777586731/project16_t5sfvb.webp"},
-    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777588334/4_k2rq2v.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills1_ydrvoc.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills2_tv9qow.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613815/Stills3_bgockw.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills4_llu9pr.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills5_i2nfbi.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills6_nwgzqk.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613816/Stills7_lfawtg.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613816/Stills8_qostxq.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613816/Stills9_o41wkr.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613815/Stills10_nuvvfh.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613815/Stills11_yapzbz.mp4"},
+    {img: "https://res.cloudinary.com/dpm7cqzhq/video/upload/q_auto/f_auto/v1777613814/Stills12_scwjay.mp4"},
+    
+    
 ];
 
 export default galleryImgs;
+
